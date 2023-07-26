00acf3f52b808c1778799151841f8dc05e61866b
KunalKB1501 <130978186+KunalKB1501@users.noreply.github.com>
2023-07-26 21:40:41 +0530

    Update BubbleSort.java

--- practice/New folder/Sorting&Searching/src/sorting/BubbleSort.java
+++ practice/New folder/Sorting&Searching/src/sorting/BubbleSort.java
@@ -19,7 +19,7 @@ public class BubbleSort {
 	}
 	public static void moreImprovedBubbleSort(int [] arr) {
 		for(int j=0;j<arr.length-1;j++) {
-		for(int i=0;i<arr.length-1-i;i++) {
+		for(int i=0;i<arr.length-1-j;i++) {
 				if(arr[i]>arr[i+1]) {
 				int temp=arr[i];
 				arr[i]=arr[i+1];
